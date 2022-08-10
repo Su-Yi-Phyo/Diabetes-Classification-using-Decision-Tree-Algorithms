56cbb5766ce4cc79203fe2fd2bc8160071462081
Su Yi Phyo <84232838+Su-Yi-Phyo@users.noreply.github.com>
2022-08-10 22:19:12 +0630

    Add files via upload

---
+++ diabetes.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import pickle
+
+model = pickle.load(open('C:\AI Projects\Diabetes classification using decision tree'),'rb')
+
+st.title("Diabetes Testing System")
+st.write("Do you want to test whether you have diabetes or not?")
+st.write("Test HERE!")
+
+glucose = st.text_input("Glucose Rate:")
+bloodpressure = st.text_input("Blood Pressure:")
+skin = st.text_input("Skin Thickness:")
+insulin = st.text_input("Insulin:")
+predigree = st.text_input("Diabetes Predigree Function:")
+age = st.text_input("Age:")
+submit = st.button("Submit")
+
+df=pd.dataframe{glucose,bloodpressure,skin,insulin,predigree,age}
+
+result=model.predict(df)
+
+if result==0:
+    st.write("You don't have diabetes. Congratulations!")
+else:
+    st.write("You have diabetes. Take care.")
