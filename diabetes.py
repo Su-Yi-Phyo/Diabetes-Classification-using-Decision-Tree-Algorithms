ae0adc246cdfcc691cb735a4174738c401b00b31
Su Yi Phyo <84232838+Su-Yi-Phyo@users.noreply.github.com>
2022-08-11 18:59:44 +0630

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib 
 
-model = joblib.load'Diabetes Classification.sav')
+model = joblib.load('Diabetes Classification.sav')
 
 st.markdown(
   f"""
